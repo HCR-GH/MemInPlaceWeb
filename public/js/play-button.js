550c6379a75894e7ad438c2b1d5a7143fa0e8771
Hattie Rowling <hattierowling@gmail.com>
2023-08-22 12:24:34 +0100

    updated play button text in 2nd location in js file

--- public/js/play-button.js
+++ public/js/play-button.js
@@ -5,7 +5,7 @@ ctrl.onclick = function () {
 
 // Update the Button
 var pause = ctrl.innerHTML === '<span class="icon-text"><span class="icon"><i class="fa-solid fa-circle-pause"></i></span><span>Pause</span></span>'; //sets pause to  true / false
-ctrl.innerHTML = pause ? '<span class="icon-text"><span class="icon"><i class="fa-solid fa-circle-play"></i></span><span>Play</span></span>' 
+ctrl.innerHTML = pause ? '<span class="icon-text"><span class="icon"><i class="fa-solid fa-circle-play"></i></span><span>Listen</span></span>'
 : '<span class="icon-text"><span class="icon"><i class="fa-solid fa-circle-pause"></i></span><span>Pause</span></span>'; //if pause is true set html to play else pause
 
 // Update the Audio
